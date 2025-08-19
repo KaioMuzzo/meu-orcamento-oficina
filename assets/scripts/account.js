badb78c6eeacf2956b49d6ec7c6b0deee903ce7e
KaioMuzzo <kaio.rmdourado@gmail.com>
2025-08-19 20:21:48 -0300

    Correção na parte logica pra verificar pagina

--- assets/scripts/account.js
+++ assets/scripts/account.js
@@ -1,11 +1,11 @@
 import { login, register } from "./firebase.js"
 
-if (window.location.pathname === "pages/cadastro.html") {
+if (window.location.pathname.includes("pages/cadastro.html")) {
     const userRegister = document.getElementById("user-register")
-
+    
     userRegister.addEventListener("submit", async (e) => {
         e.preventDefault()
-
+        
         const nameUser = document.getElementById("name-user").value
         const lastNameUser = document.getElementById("last-name-user").value
         const emailUser = document.getElementById("email-user").value
@@ -14,18 +14,19 @@ if (window.location.pathname === "pages/cadastro.html") {
         await register(capitalize(nameUser), capitalize(lastNameUser), emailUser, passwordUser)
     })
 }
-
-if (window.location.pathname === "pages/login.html") {
+console.log('teste 1')
+if (window.location.pathname.includes("pages/login.html")) {
     const userLogin = document.getElementById("user-login")
-
+    console.log('teste 1')
     userLogin.addEventListener("submit", async (e) => {
         e.preventDefault()
-
+        console.log('teste 2')
         const emailUser = document.getElementById("email-user").value
         const passwordUser = document.getElementById("password-user").value
         const success = await login(emailUser, passwordUser)
 
         if(success) {
+            console.log('teste 3')
             window.location.href = "../index.html"
         } else {
             alert("Email ou senha incorretos.")
